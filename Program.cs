44c0582bdcdcca06dcfe2eb5032ec4290c40f900
F1lton <48019969+F1lton@users.noreply.github.com>
2019-02-28 19:25:38 +0200

    Task about teacher's payment
    Teacher comes for his payment - X dollars and Y cents. But director made a mistake and gave him Y dollars and X cents. So teacher took this money, and miss 5 cents. After coming home he discovered that he got twice as much. Question: What is the payment of teacher.
    
    This code is used for checking values of X and Y.
    
    That's my first project, sent here. Hello dudes who think it's interesting.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TaskWithTeachers
+{
+    //x + 0.01y == (y + 0.01x - 0.05) / 2
+    //x + 0.01y == y/2 + 0.005x - 0.025
+    //x + 0.01y - y/2 - 0.005x = -0.025
+    //x + 1/100y - 50/100y - 1/200x = -0.025
+    //199/200x -49/100y = -0.025
+    //0.995x - 0.49y = -0.025
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, res;
+            int rozv = 0;
+            for (x = 0; x <= 100; x++)
+                for (y = 0; y <= 100; y++)
+                {                    
+                    res = (double)(0.995 * x) - (double)(0.49 * y);
+                    if (res >= -0.026 && res <= -0.024)
+                    {
+                        Console.WriteLine("\n\tx=" + x + ", y=" + y);
+                        Console.WriteLine("\t0.995 * " + x + " - 0.49 * " + y + " = " + res);
+                        Console.WriteLine("\n\tIts result, dude!(Maybe)!!\n");
+                        rozv++;
+                    }
+                }
+            Console.WriteLine("\tCount of calculates: " + rozv);
+            Console.ReadKey();
+        }
+    }
+}
